b431b4b2099091f204b2918081ee465e4e8e3715
Dave <dave@webdave.de>
2023-11-13 22:11:14 +0100

    clean up

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,8 @@
-import { enableProdMode, importProvidersFrom } from "@angular/core";
-import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { enableProdMode } from "@angular/core";
 
 import { environment } from "./environments/environment";
 import { AppComponent } from "./app/app.component";
-import { MatListModule } from "@angular/material/list";
-import { MatIconModule } from "@angular/material/icon";
-import { MatSidenavModule } from "@angular/material/sidenav";
-import { MatButtonModule } from "@angular/material/button";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { provideAnimations } from "@angular/platform-browser/animations";
-import {
-  withInterceptorsFromDi,
-  provideHttpClient,
-} from "@angular/common/http";
-import { FormsModule } from "@angular/forms";
-import {
-  BrowserModule,
-  HammerModule,
-  bootstrapApplication,
-} from "@angular/platform-browser";
-import { routes } from "./app/app.routes";
-import { provideRouter } from "@angular/router";
+import { bootstrapApplication } from "@angular/platform-browser";
 import { appConfig } from "./app/app.config";
 
 if (environment.production) {
